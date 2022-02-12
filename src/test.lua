d883af419400d39d203025e5118eab54b01ee5cc
Ryan Starrett <98286404+well-in-that-case@users.noreply.github.com>
2022-02-12 12:45:13 -0500

    messy, for speed testing mostly.

--- src/test.lua
+++ src/test.lua
@@ -1,22 +1,35 @@
-local otable = require "impl"
+local otable = require "orderedfields"
 
-local t = otable.orderedtable()
+do
+    local t = otable.orderedtable()
 
-for i = 1, 10 do
-    local k, v = "key"..tostring(i), "value"..tostring(i)
-    if otable.add(t, k, v) then
-        print(("Successfully added the following pair: %s=%s"):format(k, v))
+    for i = 1, 1000 do
+        local k, v = "key"..tostring(i), "value"..tostring(i)
+        if otable.add(t, k, v) then
+            print(("Successfully added the following pair: %s=%s"):format(k, v))
+        end
     end
+    print("Memory usage: ", collectgarbage "count")
+
+    otable.mod(t, "key1", "new", false)
+
+    t[1] = "ok"
+    t["unordered field"] = "ok2"
+
+    print("Numeric indice support: "..tostring(t[1] == "ok"))
+    print("Unordered field support: "..tostring(t["unordered field"] == "ok2"))
+
+    print("Memory usage: ", collectgarbage "count")
+    collectgarbage()
+    print("Memory usage: ", collectgarbage "count")
+    collectgarbage()
+    collectgarbage()
+    collectgarbage()
+    print("Memory usage: ", collectgarbage "count")
 end
 
-otable.mod(t, "key1", "new", false)
+collectgarbage()
+collectgarbage()
+collectgarbage()
 
-t[1] = "ok"
-t["unordered field"] = "ok2"
-
-print("Numeric indice support: "..tostring(t[1] == "ok"))
-print("Unordered field support: "..tostring(t["unordered field"] == "ok2"))
-
-for k, v in pairs(t) do
-    print(k, v)
-end
+print("Memory usage: ", collectgarbage "count")
