77b93c8c88a6d2d08d52b2336e3019f4e15bf180
Ryan Starrett <98286404+well-in-that-case@users.noreply.github.com>
2022-02-13 16:39:59 -0500

    update tests
    doesn't pertain to the package at all

--- src/test.lua
+++ src/test.lua
@@ -1,9 +1,9 @@
 local otable = require "orderedfields"
 
 do
-    local t = otable.orderedtable()
+    local t = otable.orderedtable(true)
 
-    for i = 1, 1000 do
+    for i = 1, 900 do
         local k, v = "key"..tostring(i), "value"..tostring(i)
         if otable.add(t, k, v) then
             print(("Successfully added the following pair: %s=%s"):format(k, v))
@@ -11,25 +11,18 @@ do
     end
     print("Memory usage: ", collectgarbage "count")
 
-    otable.mod(t, "key1", "new", false)
+    otable.mod(t, "key899", "newvalue", true)
+    --t.key899 = "newvalue"
+
+    for key, value in pairs(t) do
+        print(key, value)
+    end
 
     t[1] = "ok"
     t["unordered field"] = "ok2"
 
     print("Numeric indice support: "..tostring(t[1] == "ok"))
     print("Unordered field support: "..tostring(t["unordered field"] == "ok2"))
-
-    print("Memory usage: ", collectgarbage "count")
-    collectgarbage()
-    print("Memory usage: ", collectgarbage "count")
-    collectgarbage()
-    collectgarbage()
-    collectgarbage()
-    print("Memory usage: ", collectgarbage "count")
-
-    for key, value in pairs(t) do
-        print(key, value)
-    end
 end
 
 collectgarbage()
@@ -37,3 +30,25 @@ collectgarbage()
 collectgarbage()
 
 print("Memory usage: ", collectgarbage "count")
+
+local t = otable.orderedtable()
+
+for i = 1, 10 do
+    otable.add(t, "key"..tostring(i), "value"..tostring(i))
+end
+
+for i = 1, 10 do
+    local key, _ = otable.getindex(t, i, true)
+    print("Key @ Index "..tostring(i)..": "..key)
+end
+
+for i = 1, 10 do
+    print("Value @ Index "..tostring(i)..": "..tostring(otable.getindex(t, i)))
+end
+
+t.key11 = "hello world!"
+t.key12 = "hello world, again!"
+
+for k, v in t:orderediterator() do
+    print(k, v)
+end
